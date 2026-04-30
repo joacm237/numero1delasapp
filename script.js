c4d6122ec3294fe54ea7deaf02165a8c509b41bc
joacm237 <ratiofraude667@gmail.com>
2026-04-30 09:49:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-//**** MENU LATERAL ****//
-const menuToggle = document.getElementById("menu-toggle");
-const sideMenu = document.getElementById("side-menu");
-
-menuToggle.addEventListener("click", () => {
-  sideMenu.classList.toggle("active");
-});
+<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.halo.min.js"></script>
+<script>
+VANTA.HALO({
+  el: "#vanta",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  size: 2.50
+})
+</script>
